3a627ac7a741d214f9023d9babf1ec470f501afa
KELLY LUU <kellyluuu@gmail.com>
2022-06-22 20:01:07 -0500

    frontend

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import Google from "../img/google.png"
+//import Facebook from "../img/facebook.png"
+import Github from "../img/github.png"
+
+export default function Login() {
+
+    const google = () => {
+        window.open("http://localhost:3001/auth/google", "_self")
+    }
+    const github = () => {
+        window.open("http://localhost:3001/auth/github", "_self");
+      };
+    
+    //   const facebook = () => {
+    //     window.open("http://localhost:3001/auth/facebook", "_self");
+    //   };
+  return (
+    <div className='login'>
+        <h1 className="loginTitle">Choose a Login Method</h1>
+        <div className="wrapper">
+            <div className="left">
+                <div className="loginButton google"onClick={google}>
+                    <img src={Google} alt="icon" className="icon"/>
+                    Google
+                </div>
+
+                {/* <div className="loginButton facebook" onClick={facebook}>
+                    <img src={Facebook} alt="icon" className="icon"/>
+                     Facebook
+                </div> */}
+
+                <div className="loginButton github" onClick={github}>
+                    <img src={Github} alt="icon" className="icon"/>
+                     Github
+                </div>
+            </div>
+            <div className="center">
+                <div className="line"></div>
+                <div className="or">OR</div>    
+            </div>
+            <div className="right">
+                <input type="text" placeholder="Username" className="userName"/>
+                <input type="password" placeholder="Password" className="password"/>
+                <button type="submit" className="submit">Login</button>
+            </div>
+        </div>   
+    </div>
+  )
+}
---
+++ client/src/pages/Post.jsx
@@ -0,0 +1,16 @@
+import { posts } from "../data";
+import { useLocation } from "react-router-dom";
+
+export default function Post() {
+    const location = useLocation();
+    const path = location.pathname.split('/')[2];
+    const post = posts.find(post => post.id.toString() === path);
+  return (
+    <div className='post'>
+        <img src={post.img} alt="" className="postImg"/>
+        <h1 className='postTitle'>{post.title}</h1>
+        <p className='postDesc'>{post.desc}</p>
+        <p className="postLongDesc">{post.longDesc}</p>
+    </div>
+  )
+}
